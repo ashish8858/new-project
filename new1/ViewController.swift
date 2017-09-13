cc31b2774d9c798866418c9f63646e664fe99984
student <student@Admins-Mac-mini-84.local>
2017-09-13 16:10:58 +0530

    dfgds

--- new1/ViewController.swift
+++ new1/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     var x = "wxyz"
+    var y = "wxyz"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
